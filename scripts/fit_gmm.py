7f16130adc2f852d2a7479dcc3611ac60898147d
jakub karwowski <jakub.p.karwowski@gmail.com>
2025-06-22 01:03:25 +0200

    gmm fit dvc

--- scripts/fit_gmm.py
+++ scripts/fit_gmm.py
@@ -12,7 +12,6 @@ from torch_geometric.datasets import WikiCS
 import pickle
 
 
-
 def main():
     sys.path.append(str(Path(__file__).resolve().parent.parent))
     from src.gmm import GaussianMixtureModel, GMMDataModule, get_gmm_trainer
@@ -25,42 +24,48 @@ def main():
     params_path = Path(__file__).resolve().parent.parent / "params.yaml"
 
     with open(params_path, "r") as f:
-        params = yaml.safe_load(f)['wikics']['fit_gmm'][args.config]
+        params = yaml.safe_load(f)['wikics']['fit_gmm_stage'][args.config]
 
     SEED = int(params['seed'])
+    ACCELERATOR = 'cpu'
 
-    if params['encoder'] == 'none':
+    if params['input_type'] == 'raw':
         p = str(
             Path(__file__).resolve().parent.parent / 'data' / 'WikiCS'
         )
         dataset = WikiCS(root=p)
         data = dataset[0]
         node_embedings = data.x
-    elif params['encoder'] == 'gcn':
-        embedings_name = f"node_embeddings_gca_{params['encoder']}_{params['dim']}_{params['version']}.pt"
-        embedings_path = Path(__file__).resolve().parent.parent / "data" / "embeddings" / embedings_name
+    elif params['input_type'] == 'embeddings':
+        embedings_path = Path(__file__).resolve(
+        ).parent.parent / params['input']
         with open(embedings_path, 'rb') as f:
             node_embedings = torch.load(f)
     else:
         raise ValueError(params['encoder'])
 
     if params['model'] == 'sklearn':
-        model = GaussianMixture(10, max_iter=int(params['max_epochs'], tol=float(params['tol'])), random_state=SEED)
+        model = GaussianMixture(10, max_iter=int(
+            params['max_epochs'], tol=float(params['tol'])), random_state=SEED)
         model.fit(node_embedings.numpy())
-        model_file = Path(__file__).resolve().parent.parent / 'models' / 'gmm' / f"gmm_{params['encoder']}_{params['dim']}_{params['version']}.pt"
+        model_file = Path(__file__).resolve().parent.parent / 'models' / 'gmm' / \
+            f"gmm_{params['encoder']}_{params['dim']}_{params['version']}.pt"
         with open('gmm_model.pkl', 'wb') as f:
             pickle.dump(model_file, f)
     elif params['model'] == 'diy':
         datamodule = GMMDataModule(node_embedings)
         torch.manual_seed(SEED)
-        trainer = get_gmm_trainer('test_gmm', max_epochs=int(params['max_epochs']), termination_threshold=float(params['tol']))
+        trainer = get_gmm_trainer('gmm', max_epochs=int(
+            params['max_epochs']), termination_threshold=float(params['tol']), accelerator=ACCELERATOR, patience=5)
         model = GaussianMixtureModel(10, int(params['dim']))
         trainer.fit(model, datamodule=datamodule)
         os.makedirs("./models/gmm", exist_ok=True)
-        model_file = Path(__file__).resolve().parent.parent / 'models' / 'gmm' / f"gmm_{params['encoder']}_{params['dim']}_{params['version']}.pt"
+        model_file = Path(__file__).resolve().parent.parent / 'models' / 'gmm' / \
+            f"gmm_{params['encoder']}_{params['dim']}_{params['version']}.pt"
         torch.save(model.state_dict(), str(model_file))
     else:
         raise ValueError(params['model'])
+
 
 if __name__ == "__main__":
     main()
--- src/gmm.py
+++ src/gmm.py
@@ -6,23 +6,26 @@ from pytorch_lightning.callbacks import EarlyStopping
 import torch.nn as nn
 import math
 
+
 class GaussianMixtureModel(pl.LightningModule):
     def __init__(
         self,
-        n_components: int, 
+        n_components: int,
         dim: int,
+        super_smooth: bool = False,
     ):
         """Inicjalizacja modelu mikstur gausowskich.
-        
+
         :param n_components: liczba komponentów modelu
         :param n_epochs: liczba epok trenowania modelu
-        :param termination_threshold: próg przyrostu log likelihood do zatrzymywania
         """
         super().__init__()
         self.n_components = n_components
         self.dim = dim
-        
-        self.means = nn.Parameter(torch.rand((self.n_components, self.dim)), requires_grad=False)
+        self.super_smooth = super_smooth
+
+        self.means = nn.Parameter(torch.rand(
+            (self.n_components, self.dim)), requires_grad=False)
 
         self.covariances = nn.Parameter(torch.eye(self.dim).repeat(
             self.n_components, 1, 1
@@ -34,21 +37,22 @@ class GaussianMixtureModel(pl.LightningModule):
         ), requires_grad=False)
 
     @torch.no_grad()
-    def gamma_top(self, x:torch.Tensor) -> torch.Tensor:
+    def gamma_top(self, x: torch.Tensor) -> torch.Tensor:
         n_points = x.shape[0]
-        #https://stats.stackexchange.com/questions/390532/adding-a-small-constant-to-the-diagonals-of-a-matrix-to-stabilize
-        cov_reg = self.covariances + 1e-6 * torch.eye(self.dim, device=x.device).unsqueeze(0) 
+        # https://stats.stackexchange.com/questions/390532/adding-a-small-constant-to-the-diagonals-of-a-matrix-to-stabilize
+        cov_reg = self.covariances + 1e-6 * \
+            torch.eye(self.dim, device=x.device).unsqueeze(0)
         xs = torch.reshape(x, (1, n_points, self.dim, 1))
         mus = torch.reshape(self.means, (self.n_components, 1, self.dim, 1))
-        Sigmas = torch.reshape(cov_reg, (self.n_components, 1, self.dim, self.dim))    
-        
+        Sigmas = torch.reshape(
+            cov_reg, (self.n_components, 1, self.dim, self.dim))
 
         log_py = torch.log(self.mixing_coefs + 1e-10).unsqueeze(1)
         diff = xs - mus
         inv_Sigmas = torch.linalg.inv(Sigmas)
 
         log_top = torch.matmul(
-            torch.matmul(diff.transpose(3, 2), inv_Sigmas), 
+            torch.matmul(diff.transpose(3, 2), inv_Sigmas),
             diff
         ).squeeze(-1).squeeze(-1)
 
@@ -57,9 +61,14 @@ class GaussianMixtureModel(pl.LightningModule):
         log_pxy = log_bottom - 0.5 * log_top
         log_pyx = log_py + log_pxy
         # Normalize probabilities
-        p_yx = torch.exp(log_pyx )#- torch.logsumexp(log_pyx, dim=0, keepdim=True)
+        # - torch.logsumexp(log_pyx, dim=0, keepdim=True)
+        if self.super_smooth:
+            p_yx = torch.exp(
+                log_pyx - torch.logsumexp(log_pyx, dim=0, keepdim=True))
+        else:
+            p_yx = torch.exp(log_pyx)
         return p_yx
-    
+
     @torch.no_grad()
     def expectation_step(self, x: torch.Tensor) -> torch.Tensor:
         """Krok Expectation (obliczenie responsibilities)."""
@@ -69,7 +78,7 @@ class GaussianMixtureModel(pl.LightningModule):
     @torch.no_grad()
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.expectation_step(x)
-    
+
     @torch.no_grad()
     def maximization_step(
         self, x: torch.Tensor, responsibilities: torch.Tensor
@@ -78,37 +87,41 @@ class GaussianMixtureModel(pl.LightningModule):
         N_k = torch.sum(responsibilities, 1)
         means = ((responsibilities.unsqueeze(1) @ x).squeeze(1).T / N_k).T
         n_points = x.shape[0]
-        mus = torch.reshape(x, (1, n_points, self.dim, 1)) - torch.reshape(self.means, (self.n_components, 1, self.dim, 1))
-        covariances = responsibilities.unsqueeze(-1).unsqueeze(-1) * (mus @ mus.transpose(3, 2))
+        mus = torch.reshape(x, (1, n_points, self.dim, 1)) - \
+            torch.reshape(self.means, (self.n_components, 1, self.dim, 1))
+        covariances = responsibilities.unsqueeze(
+            -1).unsqueeze(-1) * (mus @ mus.transpose(3, 2))
         covariances = torch.sum(covariances, 1) / N_k.reshape(-1, 1, 1)
         mixing_coefs = N_k / n_points
         self.means.data.copy_(means)
         self.covariances.data.copy_(covariances)
         self.mixing_coefs.data.copy_(mixing_coefs)
 
-    @torch.no_grad()    
+    @torch.no_grad()
     def loglikelihood(self, x: torch.Tensor) -> float:
         """Log-likelihood modelu."""
         g = self.gamma_top(x)
         return (torch.sum(
-            torch.log(torch.sum(g, 0)) 
+            torch.log(torch.sum(g, 0))
         ) / x.shape[0]).item()
-    
+
     @torch.no_grad()
     def training_step(self, x: torch.Tensor):
         responsibilities = self.expectation_step(x)
         self.maximization_step(x, responsibilities)
         log_likelihood = self.loglikelihood(x)
-        self.log('log-likelihood', log_likelihood, on_epoch=True, on_step=False)
+        self.log('log-likelihood', log_likelihood,
+                 on_epoch=True, on_step=False)
 
     def configure_optimizers(self):
         return None
 
-def get_gmm_trainer(model_name: str, max_epochs: int, patience: int = 1, termination_threshold: float = 1e-3):
-     p = str(Path(__file__).resolve().parent.parent / "data" / "gmm-logs")
-     return pl.Trainer(
+
+def get_gmm_trainer(model_name: str, max_epochs: int, patience: int = 1, termination_threshold: float = 1e-3, accelerator: str = "gpu"):
+    p = str(Path(__file__).resolve().parent.parent / "data" / "gmm-logs")
+    return pl.Trainer(
         max_epochs=max_epochs,
-        accelerator="gpu",
+        accelerator=accelerator,
         logger=pl.loggers.TensorBoardLogger(
             save_dir=p,
             name=model_name,
@@ -118,27 +131,31 @@ def get_gmm_trainer(model_name: str, max_epochs: int, patience: int = 1, termina
         num_sanity_val_steps=0,
         callbacks=[
             EarlyStopping(
-                monitor="log-likelihood",   
-                min_delta=termination_threshold,         
-                patience=patience,             
+                monitor="log-likelihood",
+                min_delta=termination_threshold,
+                patience=patience,
                 verbose=True,
-                mode="max"             
-        )]
+                mode="max"
+            )]
     )
+
 
 class WholeTensorDataset(torch.utils.data.Dataset):
     def __init__(self, tensor):
         self.tensor = tensor
+
     def __len__(self):
         return len(self.tensor)
+
     def __getitem__(self, idx):
         return self.tensor[idx]
+
 
 class GMMDataModule(pl.LightningDataModule):
     def __init__(self, tensor):
         super().__init__()
         self.tensor = tensor
+
     def train_dataloader(self):
         dataset = WholeTensorDataset(self.tensor)
         return torch.utils.data.DataLoader(dataset, batch_size=len(dataset), shuffle=False)
-        
